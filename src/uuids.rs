98b56fd8e54600db616c3322df093db89fa8685e
KavanPrice <kavanprice@gmail.com>
2024-05-22 17:21:17 +0100

    Remove deprecated UUID

--- src/uuids.rs
+++ src/uuids.rs
@@ -35,7 +35,6 @@ pub mod schema {
 pub mod service {
     pub const DIRECTORY: &str =             "af4a1d66-e6f7-43c4-8a67-0fa3be2b1cf9";
     pub const CONFIG_DB: &str =             "af15f175-78a0-4e05-97c0-2a0bb82b9f3b";
-    pub const REGISTRY: &str =              "af15f175-78a0-4e05-97c0-2a0bb82b9f3b";
     pub const AUTHENTICATION: &str =        "cab2642a-f7d9-42e5-8845-8f35affe1fd4";
     pub const COMMAND_ESCALATION: &str =    "78ea7071-24ac-4916-8351-aa3e549d8ccd";
     pub const MQTT: &str =                  "feb27ba3-bd2c-4916-9269-79a61ebc4a47";
